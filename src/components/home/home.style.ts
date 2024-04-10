e01fcc096e22e88585f580959b2793440ba1e115
Kot Fil <filippkotenko@outlook.com>
2024-04-10 14:45:32 +0300

    home page

--- src/components/home/home.style.ts
+++ src/components/home/home.style.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 // import img from '../images/background/background.webp'
+import Image from 'react-image-webp';
+import {device} from "../styles/adaptive/device";
 
 export const Wrapper = styled.div`
   margin-top: 50px;
@@ -9,9 +11,43 @@ export const Wrapper = styled.div`
 `
 
 export const Title = styled.h1`
-  
+  font-size: 55px;
+  font-weight: bold;
 `
-export const Text = styled.p`
-  
+export const Text = styled.div`
+  width: 100%;
+  top: 10%;
+  left: 25%;
+  transform: translateX(-25%);
+  text-align: center;
+  position: absolute;
+  @media ${device.mobileS} {
+    top: -3%;
+  }
+  @media ${device.mobileM} {
+    top: 8%;
+  }
+  @media ${device.mobileL} {
+    top: 4%;
+  }
+  @media ${device.tablet} {
+    top: 20%;
+  }
+  @media ${device.laptop} {
+    top: 20%;
+  }
+  @media ${device.laptop} {
+    top: 15%;
+  }
+  @media ${device.laptopL} {
+    top: 25%;
+  }
+  @media ${device.desktop} {
+    top: 25%;
+  }
+`;
+
+export const ImageWrap = styled.div`
+  position: relative;
 `
 
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,17 +1,23 @@
 import {useTranslation} from "react-i18next";
-import {Wrapper,Title,Text} from "./home.style";
+import {ImageWrap, Text, Title, Wrapper} from "./home.style";
 import {PrimaryButton} from "../button/button";
 import {Typography} from "../typography/typography";
-// @ts-ignore
-import Image from '../images/background/background.webp'
+import Image from 'react-image-webp';
 
 const Home = () => {
     const {t}=useTranslation()
     return (
         <Wrapper>
-          <Title><Typography type='interMedium20'>{t('home_title')}</Typography></Title>
-            <Text><Typography type='interRegular14'>{t('home_text')}</Typography></Text>
-            <PrimaryButton>Learn</PrimaryButton>
+            <ImageWrap>
+            <Image   src={require('../images/background/background.webp')}
+                         webp={require('../images/background/background3.webp')}/>
+            </ImageWrap>
+            <Text>
+            <Title><Typography type='interMedium20'>{t('home_title')}</Typography></Title>
+          <Typography type='interRegular14'>{t('home_text')}</Typography>
+           <div> <PrimaryButton>Learn</PrimaryButton></div>
+            </Text>
+
         </Wrapper>
     );
 };
