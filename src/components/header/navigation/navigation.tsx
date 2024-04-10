8567ffdadbed44183f84f5597e9ba64c9d90a82d
Kot Fil <filippkotenko@outlook.com>
2024-04-10 10:08:56 +0300

    nav

--- src/components/header/navigation/navigation.tsx
+++ src/components/header/navigation/navigation.tsx
@@ -14,12 +14,16 @@ const Navigation = () => {
     }
 
     return <Tabs   sx={{marginLeft: 'auto'}} value={value} onChange={handleChange}>
+        {/*//@ts-ignore*/}
         <NavLink to={'/'}> <TabWrap textColor="secondary" value={t('navbar_home')} label={<TypographyWrap
             type={'montserratSemiBold18'}>  {t('navbar_home')}</TypographyWrap>}/></NavLink>
+        {/*//@ts-ignore*/}
         <NavLink to={'/price'}> <TabWrap textColor="secondary" value={t('navbar_price')} label={<TypographyWrap
             type={'montserratSemiBold18'}>{t('navbar_price')}  </TypographyWrap>}/></NavLink>
+        {/*//@ts-ignore*/}
         <NavLink to={'/about'}> <TabWrap textColor="secondary" value={t('navbar_about')} label={<TypographyWrap
             type={'montserratSemiBold18'}>  {t('navbar_about')} </TypographyWrap>}/></NavLink>
+        {/*//@ts-ignore*/}
         <NavLink to={'/contacts'}> <TabWrap textColor="secondary" value={t('navbar_contacts')} label={<TypographyWrap
             type={'montserratSemiBold18'}> {t('navbar_contacts')}</TypographyWrap>}/></NavLink>
     </Tabs>
